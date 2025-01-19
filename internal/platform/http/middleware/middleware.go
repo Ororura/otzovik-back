a6d605a7d923af0259e0a78acf42fb5b649f5ac1
Ororura <hitmanlol890@gmail.com>
2025-01-19 14:10:02 +0300

    feat(middleware): add CORS support and update dependencies

--- internal/platform/http/middleware/middleware.go
+++ internal/platform/http/middleware/middleware.go
@@ -4,25 +4,35 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Logger() gin.HandlerFunc {
-    return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-        return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
-            param.ClientIP,
-            param.TimeStamp.Format(time.RFC1123),
-            param.Method,
-            param.Path,
-            param.Request.Proto,
-            param.StatusCode,
-            param.Latency,
-            param.Request.UserAgent(),
-            param.ErrorMessage,
-        )
-    })
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	})
 }
 
 func Recovery() gin.HandlerFunc {
-    return gin.Recovery()
-} 
+	return gin.Recovery()
+}
+
+func CORS() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+}
